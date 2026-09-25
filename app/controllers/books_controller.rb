3d5d95125a62ad7fd0604f2e712897b5350c6391
2411140051w-blip <2411140051w@ed.fuk.kindai.ac.jp>
2026-09-25 02:34:09 +0000

    応用課題完成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,15 +9,23 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(title: params[:book][:title], author: params[:book][:author], published_year: params[:book][:published_year])
-    book.save
-    redirect_to '/' #=>一覧ページにリダイレクトする
+    @book = Book.new(title: params[:book][:title], author: params[:book][:author], published_year: params[:book][:published_year])
+    if @book.save
+      flash[:notice] = '１レコード追加しました'
+      redirect_to '/' #=>一覧ページにリダイレクトする
+    else
+      render 'new', status: :unprocessable_content
+    end
   end
 
   def destroy
-    book = Book.find(params[:id])
-    book.destroy
-    redirect_to '/'
+    @book = Book.find(params[:id])
+    if @book.destroy
+      flash[:notice] = '１レコード削除しました'
+      redirect_to '/'
+    else
+      flash[:notice] = '削除に失敗しました'
+    end
   end
 
   def show
@@ -29,8 +37,14 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(title: params[:book][:title], author: params[:book][:author], published_year: params[:book][:published_year])
-    redirect_to '/'
+    @book = Book.find(params[:id])
+    if @book.update(title: params[:book][:title], author: params[:book][:author], published_year: params[:book][:published_year])
+      flash[:notice] = '１レコード編集しました'
+      redirect_to '/'
+    else
+      flash[:notice] = '編集に失敗しました'
+      render 'edit', status: :unprocessable_content
+    end
   end
+
 end
